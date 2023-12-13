51d0e7fa503d6d9d444ec5e15abe47dd85362083
Juan Martín Diez <diez.juan.martin12@gmail.com>
2023-12-13 14:41:04 -0300

    entrega final react

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,7 +1,7 @@
 import { db } from "../db/db";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
-export const getCategories = async () => {
+/*export const getCategories = async () => {
   try {
     const categoriesRef = collection(db, "getCategories");
     const response = await getDocs(categoriesRef);
@@ -24,5 +24,28 @@ export const getCategoryBySlug = async (slug) => {
   }
 };
 
-export default { getCategories, getCategoryBySlug };
+export default { getCategories, getCategoryBySlug };*/
 
+const url = "https://64de8919825d19d9bfb2b059.mockapi.io/getCategories";
+
+export const getCategories = async () => {
+  try {
+    const response = await fetch(url);
+    const categories = await response.json();
+    return categories;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCategoryBySlug = async (slug) => {
+  try {
+    const response = await fetch(url);
+    const categories = await response.json();
+    return categories.find((category) => category.slug === slug);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default { getCategories, getCategoryBySlug };
